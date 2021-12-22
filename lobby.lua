eafff35e309545564f991d8aabe60ab2a2e58f31
Ciaran <ravenreju@gmail.com>
2021-12-22 12:29:52 +0000

    added lobby.lua

---
+++ lobby.lua
@@ -0,0 +1,46 @@
+-- // Variables
+local LocalPlayer = game:GetService("Players").LocalPlayer;
+local MainFunction = game:GetService("ReplicatedStorage").RemoteFunction;
+local TDS = {};
+
+-- // Functions
+function TDS:RemoveTower(Tower)
+    local args = {
+        [1] = "Inventory",
+        [2] = "Execute",
+        [3] = "Troops",
+        [4] = "Remove",
+        [5] = {
+            ["Name"] = Tower
+        }
+    }
+
+    MainFunction:InvokeServer(unpack(args))
+end
+
+function TDS:EquipTower(Tower)
+    local args = {
+        [1] = "Inventory",
+        [2] = "Execute",
+        [3] = "Troops",
+        [4] = "Add",
+        [5] = {
+            ["Name"] = Tower
+        }
+    }
+    wait()
+    MainFunction:InvokeServer(unpack(args))
+end
+
+function TDS:FetchTowers()
+    local args = {
+        [1] = "Profiles",
+        [2] = "Download",
+        [3] = LocalPlayer
+    }
+
+    local Profile = MainFunction:InvokeServer(unpack(args))
+    return Profile.Troops
+end
+
+return TDS;
